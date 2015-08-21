70e094927907e65f4adac4c0a66706d9cb2d766e
landal79 <alex.landini@gmail.com>
2015-08-21 02:30:41 +0200

    fixed issues

---
+++ tasks/maven-effective-pom.js
@@ -0,0 +1,37 @@
+/*
+ * grunt-maven-effective-pom
+ *
+ * Copyright (c) 2015 Alex Landini, contributors
+ * Licensed under the MIT license.
+ */
+
+//'use strict';
+
+var cp = require('child_process');
+var fs = require('fs');
+var xml2js = require('xml2js');
+
+module.exports = function (grunt) {
+
+    grunt.registerMultiTask('mavenEffectivePom', 'Loads effective maven pom', function () {
+
+        var options = this.options({
+            file : "target/effective-pom.xml"
+        });
+
+        grunt.verbose.writeflags(options, "Options");
+
+        var parser = new xml2js.Parser({explicitArray: false});
+        var pom = null;
+        parser.addListener('end', function (result) {
+            grunt.config.set('pom', result);
+            grunt.verbose.writeln('loaded maven pom for project: ' + grunt.config('pom.project.artifactId'));
+        });
+
+        cp.execSync('mvn help:effective-pom -Doutput=' + options.file);
+        parser.parseString(fs.readFileSync(options.file, grunt.file.defaultEncoding));
+
+    });
+
+};
+
